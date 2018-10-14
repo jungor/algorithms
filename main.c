5d474352e2012d38ae946866dfd1dcce03c8f561
李俊杰 <287150625@qq.com>
2018-10-14 22:54:06 +0800

    使用cmake管理构建

--- main.c
+++ main.c
@@ -1,20 +1,6 @@
-#include "utils.h"
-#include "chap02/insertion_sort.h"
-#include "chap02/merge_sort.h"
-#include "chap04/maximum_subarray.h"
-#include "chap06/heap_sort.h"
-#include "chap07/quick_sort.h"
-#include "chap08/counting_sort.h"
-#include "chap08/radix_sort.h"
-#include "chap09/order_statistic.h"
-#include "chap15/cut_rod.h"
-#include "chap15/matrix_chain_multiplication.h"
-#include "chap15/longest_common_subsequence.h"
-#include "chap15/optimal_binary_search_tree.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 int main() {
+    printf("Nothing is here. Please run \"test_algrithms\"\n");
     return 0;
 }
